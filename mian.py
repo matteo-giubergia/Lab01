139da2ce8d70469e420ca5ef87799ddccadbf3ab
matteo-giubergia <matteo.giuebrgia2003@gmail.com>
2025-03-01 14:20:04 +0100

    istanziato gli oggetti

---
+++ mian.py
@@ -0,0 +1,9 @@
+class Domanda():
+    def __init__(self, livello, domanda, risposte = []):
+        self.livello = livello
+        self.domanda = domanda
+        self.risposte = risposte
+
+
+
+
---
+++ player.py
@@ -0,0 +1,4 @@
+class Player():
+    def __init__(self, punteggio, levMax):
+        self.punteggio = punteggio
+        self.levMax = levMax
